7534da4f2e31581e1dd5c919b24574fe59cdfaf7
Crowdbotics <team@crowdbotics.com>
2021-03-07 09:31:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/milestones_24897/MainActivity.java
@@ -0,0 +1,15 @@
+package com.milestones_24897;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "milestones_24897";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://milestones-24897.botics.co",
+  defaultTimeout: 5000
+};
